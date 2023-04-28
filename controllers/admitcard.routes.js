62890834803e63582c3d531d579de0a954b16a55
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-04-28 23:50:41 +0530

    added already existing check

--- controllers/admitcard.routes.js
+++ controllers/admitcard.routes.js
@@ -8,32 +8,42 @@ admitcardRouter.get("/", (req, res) => {
 
 admitcardRouter.post("/getadmitcard", (req, res) => {
   const { name, phone, school, _class, roll_no, address } = req.body;
-  const postQuery =
-    "INSERT INTO admitcard(name, phone, school, class, roll_no, address) VALUES (?,?,?,?,?,?)";
 
-  db.query(
-    postQuery,
-    [name, phone, school, _class, roll_no, address],
-    (err, result) => {
-      if (err) {
-        res.send({ status: false, error: err.sqlMessage });
-      } else {
-        // console.log(result);
-        let getQuery = `SELECT * FROM admitcard WHERE roll_no=?`;
-        db.query(getQuery, roll_no, (err1, result1) => {
-          if (err1) {
-            res.send({ status: false, error: err.sqlMessage });
-          } else {
-            res.send({
-              status: true,
-              mssg: "Information added sucessfully",
-              admitcardInfo: result1[0],
-            });
-          }
-        });
-      }
+  const getQuery1 = `SELECT * FROM admitcard WHERE roll_no=?`;
+  db.query(getQuery1, roll_no, (err, result) => {
+    // if already existing
+    if (result.length > 0) {
+      res.send({
+        status: true,
+        mssg: "Admitcard generated sucessfully",
+        admitcardInfo: result[0],
+      });
     }
-  );
+    // If non existing
+    else {
+      const postQuery =
+        "INSERT INTO admitcard(name, phone, school, class, roll_no, address) VALUES (?,?,?,?,?,?)";
+      let queryArr = [name, phone, school, _class, roll_no, address];
+      db.query(postQuery, queryArr, (err, result) => {
+        if (err) {
+          res.send({ status: false, error: err.sqlMessage });
+        } else {
+          let getQuery2 = `SELECT * FROM admitcard WHERE roll_no=?`;
+          db.query(getQuery2, roll_no, (err1, result1) => {
+            if (err1) {
+              res.send({ status: false, error: err.sqlMessage });
+            } else {
+              res.send({
+                status: true,
+                mssg: "Information added and genrated admitcard sucessfully",
+                admitcardInfo: result1[0],
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 module.exports = {
