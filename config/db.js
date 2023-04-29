d090d4b2a636684ebb3caaed573f7b5d0189ea80
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-04-29 15:38:31 +0530

    added env

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { createPool } = require("mysql2");
 
 const db = createPool({
-  host: process.env.HOST || "localhost",
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE || "admitcarddb",
-  connectionLimit: 10,
 });
 
 module.exports = {
